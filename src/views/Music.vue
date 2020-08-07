e81eb9e4a8d1ae93f55af14ca0cf4169ac278170
Anillc <a2488953597@live.cn>
2020-08-07 14:51:15 +0800

    update music page

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>www</div>
+  <v-container class="fill-height">
+    <v-row align="baseline" justify="center" class="fill-height">
+      <v-col>
+        <v-card class="mb-8">
+          <v-card-title>关于本页</v-card-title>
+          <v-card-text>
+            本页面由<a href="https://blog.lotywe.cc">lotywe</a>提供w
+            <br/>使用OneManager提供OneDrive的直链下载
+          </v-card-text>
+        </v-card>
+        <v-card>
+          <v-card-text>
+            <iframe src="https://awsl.ee" frameborder="0" class="ifr"></iframe>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="coffee">
@@ -9,5 +26,8 @@ module.exports =
 </script>
 
 <style lang="less" scoped>
-
+.ifr {
+  width: 100%;
+  height: 500px;
+}
 </style>
